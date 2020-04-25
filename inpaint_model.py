39c42fb33b01ae667553f9e7cf60b2109ca5be28
Garima <73garima@gmail.com>
2020-04-25 03:07:02 -0700

    rough inpaint model structure

--- inpaint_model.py
+++ inpaint_model.py
@@ -1,10 +1,12 @@
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from dcgan_model import Generator,Discriminator,weights_init
 
 device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+# DCGAN parameters
 
 params = {
     "bsize" : 128,# Batch size during training.
@@ -18,22 +20,79 @@ params = {
     'beta1' : 0.5,# Beta1 hyperparam for Adam optimizer
     'save_epoch' : 2 }
 
-netG = Generator(params).to(device)
-netD = Discriminator(params).to(device)
-
-optimizerD = optim.Adam(netD.parameters(), lr=params['lr'], betas=(params['beta1'], 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=params['lr'], betas=(params['beta1'], 0.999))
-
-
-filename = "pretrained_model.pth"
-if os.path.isfile(filename):
-    saved_model = torch.load(filename, map_location=torch.device('cpu'))
-    netG.load_state_dict(saved_model['generator'])
-    netD.load_state_dict(saved_model['discriminator'])
-    optimizerD.load_state_dict(saved_model['optimizerD'])
-    optimizerG.load_state_dict(saved_model['optimizerG'])
-    params = saved_model['params']
-    print("Parameters for this Model - \n", params)
+real_label = 1
+fake_label = 0
+# Initialize BCELoss function for dcgan
+criterion = nn.BCELoss()
 
 # Start inpainting
+
+class Inpaint:
+    def __init__(self):
+        # Initialize the DCGAN model and optimizers
+        self.netG = Generator(params).to(device)
+        self.netD = Discriminator(params).to(device)
+
+        filename = "pretrained_model.pth"
+        if os.path.isfile(filename):
+            saved_model = torch.load(filename, map_location=torch.device('cpu'))
+            self.netG.load_state_dict(saved_model['generator'])
+            self.netD.load_state_dict(saved_model['discriminator'])
+            params = saved_model['params']
+        
+        self.batch_size = 64 # Batch size for inpainting
+        self.image_size = params['imsize'] # 64
+        self.num_channels = params['nc'] # 3
+        self.z_dim = params['nz'] # 100
+        self.nIters = 3000 # Iterations 
+        self.lamda = 0.2
+        self.z = torch.randn(self.batch_size, self.z_dim, 1, 1, device=device)
+        self.opt = torch.optim.Adam(self.z, lr = 0.0003)
+
+    def get_mask(self,images):
+        # Create mask according to the input image
+
+        return 0
+
+    def preprocess(self,images,mask):
+        # preprocess the images and masks
+
+        return 0
+
+    def get_imp_weighting(self, mask):
+        # Implement eq 3
+
+        return 0
+
+    def run_dcgan(self,z_i):
+        G_z_i = self.netG(z_i)
+        label = torch.full((self.batch_size,), real_label, device=device)
+        D_G_z_i = self.netD(G_z_i)
+        errG = criterion(D_G_z_i, label)
+
+        return G_z_i, errG
+
+    def get_context_loss(self, G_z_i, images, mask):
+        # Calculate context loss
+        # Implement eq 4
+        W = self.get_imp_weighting(mask)
+        # TODO: verify norm output. Its probably a vector. we need a single value
+        context_loss = torch.norm(torch.mul(W, G_z_i - images), p=1) 
+
+        return context_loss
+
+    def generate_z_hat(self, images, mask):
+        # Backpropagation for z
+        for i in range(self.nIters):
+            self.G_z_i, self.errG = self.run_dcgan(self.z)
+            self.perceptual_loss = self.errG
+            self.context_loss = self.get_context_loss(self.G_z_i, images, mask)
+            self.loss = self.context_loss + (self.lamda * self.perceptual_loss)
+            grad = torch.autograd.grad(self.loss, self.z)
+
+            # Update z
+            # https://github.com/moodoki/semantic_image_inpainting/blob/extensions/src/model.py#L182
+            
+            # TODO: Not sure if this next would work to update z. Check
+            self.opt.step() 
 
